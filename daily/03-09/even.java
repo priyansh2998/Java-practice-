0bbd8cc46bd31303d664497e1837e9aaab65a1e3
Priyansh Chauhan <80479962+priyansh2998@users.noreply.github.com>
2022-09-03 23:12:13 +0530

    Update even.java
    from a leetcode question

--- daily/03-09/even.java
+++ daily/03-09/even.java
@@ -16,7 +16,7 @@ public class even {
         int count=0;
         for(int i=0;i<5;i++){
             if(arr[i]%2==0){
-                count+=1;
+                count+=1; //taking the even number encountered in count
             }
         }
         return count;
