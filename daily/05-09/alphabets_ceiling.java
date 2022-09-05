331ef9d464c9cb87f23baadb24e612205e6da0e9
Priyansh Chauhan <80479962+priyansh2998@users.noreply.github.com>
2022-09-05 20:40:23 +0530

    Create alphabets_ceiling.java

---
+++ daily/05-09/alphabets_ceiling.java
@@ -0,0 +1,30 @@
+package dsa;
+
+import java.util.Scanner;
+
+public class alphabets_ceiling {
+    public static void main(String[] args) {
+        char[] letter={'c','f','j'};
+        char target='a';
+        System.out.println("the ceiling for " +target+" is "+ceilng(letter,target));
+    }
+    static char ceilng(char[] letter,char target){
+        int start=0;int end=letter.length-1;
+        if(target<letter[start]){
+            return 'j';
+        }
+        else if(target>letter[end]){
+            return 'c';
+        }
+        while(start>=end){
+            int mid=(start+end)/2;
+            if (target < letter[mid]) {
+                end=mid-1;
+            }else if(target > letter[mid]){
+                start=mid+1;
+            }else if(target==letter[mid]){
+                return letter[mid];
+            }
+        }return letter[start];
+    }
+}
