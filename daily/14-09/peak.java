d749b15d9ad55124ea88829ccd54bbc803f3e045
Priyansh Chauhan <80479962+priyansh2998@users.noreply.github.com>
2022-09-14 20:33:55 +0530

    Update peak.java

--- daily/14-09/peak.java
+++ daily/14-09/peak.java
@@ -2,20 +2,19 @@ package dsa;
 
 public class peak {
     public static void main(String[] args) {
-        int[] arr={1,2,4,8,11,99,9,8,3};
+        int[] arr={0,2,1,0};
         System.out.println("the peak number is "+mountainum(arr));
     }
     static int mountainum(int[] arr){
-        int mid=0;
         int start=0;
         int end =arr.length-1;
-        while(start<=end){
-            mid=start+(end-start)/2;
+        while(start<end){
+            int mid=start+(end-start)/2;
             if(arr[mid]<arr[mid+1]){
                 start=mid+1;
             }if(arr[mid]>arr[mid+1]){
-                end=mid-1;
+                end=mid;
             }
-        }return arr[mid];
+        }return arr[start];
     }
 }
