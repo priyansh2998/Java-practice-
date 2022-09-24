1f7272439c699ff966c6b5f8d1b277739cdbdd5d
Priyansh Chauhan <80479962+priyansh2998@users.noreply.github.com>
2022-09-24 17:12:10 +0530

    Create missing_num.java

---
+++ daily/24-09/missing_num.java
@@ -0,0 +1,30 @@
+package dsa;
+
+public class missing_num {
+    public static void main(String[] args) {
+        int[] arr={0,1};
+        System.out.println("the missing number is "+ missing(arr));
+    }
+    static int missing(int[] arr){
+        cyclic(arr);
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=i){
+                return i;
+            }
+        }return arr.length;
+    }
+    static void cyclic(int[] arr){
+        int i=0;
+        int a=arr.length;
+        while(i<a){
+            if(arr[i]!=i && arr[i]<a){
+                int temp=arr[arr[i]];
+                arr[arr[i]]=arr[i];
+                arr[i]=temp;
+            }
+            if(arr[i]==a || arr[i]==i){
+                i++;
+            }
+        }
+    }
+}
